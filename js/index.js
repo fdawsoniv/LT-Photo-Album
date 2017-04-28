48dd4f911410adef27533aedf5353851df5c44b1
Fredrick W Dawson IV <fdawsoniv@gmail.com>
2017-04-28 17:43:47 -0500

    Update index.js
    console.assertions

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+    // test - see console to prove that non-integers, and integers outside the accepted range will fail the test as expected
+    var input = "a";
+    console.assert(Number.isInteger(input) && (input >= 1 && input <= 100), "non-numbers will not be accepted");
+    input = "#";
+    console.assert(Number.isInteger(input) && (input >= 1 && input <= 100), "special characters will not be accepted");
+    input = 105;
+    console.assert(Number.isInteger(input) && (input >= 1 && input <= 100), "numbers over 100 will fail");
+    input = 2.5;
+    console.assert(Number.isInteger(input) && (input >= 1 && input <= 100), "decimals will also fail");
+    input = 5;
+    console.assert(Number.isInteger(input) && (input >= 1 && input <= 100), "whole numbers between 1 and 100 are A-OK");
+  // end test section
+  
     $("#getAlbum").on("click", function(){
       var input = Number(document.querySelector('#album').value);
       if (!Number.isInteger(input) || input < 1 || input > 100) {
